f7e96fa94aa63016455d713f27dc3dfc45c99fa6
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-08-09 11:09:35 +0300

    Add files via upload

---
+++ 03.AutomationTesting/05.UnitTesting/07. StringReverseTests/StringReverseTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "";
+        string  expected = string.Empty;
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        // Arrange
+        string input = "666";
+        string expected = "666";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "123";
+        string expected = "321";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ 03.AutomationTesting/05.UnitTesting/08. FactorialTests/FactorialTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void Test_CalculateFactorial_InputZero_ReturnsOne()
+    {
+        // Arrange
+        int input = 0;
+        int expected = 1;
+
+        // Act
+        int result = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        // Arrange
+        int input = 5;
+        int expected = 120;
+
+        // Act
+        int result = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
